073337b1a96ea3c3522b56daf1cd1a3c44e4a9ac
likhitha0903 <likhithabehara15@gmail.com>
2025-11-19 10:38:24 +0530

    added tag component

--- propsassignment/src/components/Tag.jsx
+++ propsassignment/src/components/Tag.jsx
@@ -1,11 +1,13 @@
 
 
 function Tag(props){
-    let {label,skills}=props
+    let {label,type,score}=props
     return(
         <>
-        <div>
-            
+        <div className={`card w-25 p-3 m-3 ${score>85?"border border-success border-5":"border border-dark-subtle"}`}>
+            <p>{`Name:${label}`}</p>
+            <p>{`Skills:${type}`}</p>
+            <p></p>
         </div>
         </>
     )
--- propsassignment/src/components/TeamCard.jsx
+++ propsassignment/src/components/TeamCard.jsx
@@ -4,15 +4,14 @@ function TeamCard(props){
     let {name,role,experience,skills,aiScore,status}=props
     return(
         <>
-        <div className={`card m-3 p-3 ${status=="active"?"text-bg-success": status=="bench"?"text-bg-primary":"text-bg-warning"}
-        ${aiScore > 85 ? "border border-3 shadow" : "border border-0" }`}>
+        <div className={`card m-3 p-3 ${status=="active"?"text-bg-success": status=="bench"?"text-bg-primary":"text-bg-warning"}`}>
             <p>{`Name: ${name}`}</p>
             <p>{`Role: ${role}`}</p>
             <p>{`Experience: ${experience}`}</p>
             <p>{`AI Score: ${aiScore}`}</p>
             <p>{`Skills: ${skills}`}</p>
         </div>
-        <Tag label={name} type={skills} />
+        <Tag label={name} type={skills} score={aiScore} />
         </>
     )
 }
